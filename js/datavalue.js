629f57d0098ad379d9dd9b62ac00cb7650b50183
Geek0599 <59893482+Geek0599@users.noreply.github.com>
2022-01-04 16:46:35 +0200

    Add files via upload

---
+++ js/datavalue.js
@@ -0,0 +1,56 @@
+	//FIELDS
+	$('input,textarea').focus(function(){
+		if($(this).val() == $(this).attr('data-value')){
+				$(this).addClass('focus');
+				$(this).parent().addClass('focus');
+			if($(this).attr('data-type')=='pass'){
+				$(this).attr('type','password');
+			};
+			$(this).val('');
+		};
+		removeError($(this));
+	});
+	$('input[data-value], textarea[data-value]').each(function() {
+		if (this.value == '' || this.value == $(this).attr('data-value')) {
+			this.value = $(this).attr('data-value');
+			if($(this).hasClass('l') && $(this).parent().find('.form__label').length==0){
+				$(this).parent().append('<div class="form__label">'+$(this).attr('data-value')+'</div>');
+			}
+		}
+		if(this.value!=$(this).attr('data-value') && this.value!=''){
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			if($(this).hasClass('l') && $(this).parent().find('.form__label').length==0){
+				$(this).parent().append('<div class="form__label">'+$(this).attr('data-value')+'</div>');
+			}
+		}
+
+		$(this).click(function() {
+			if (this.value == $(this).attr('data-value')) {
+				if($(this).attr('data-type')=='pass'){
+					$(this).attr('type','password');
+				};
+				this.value = '';
+			};
+		});
+		$(this).blur(function() {
+			if (this.value == '') {
+				this.value = $(this).attr('data-value');
+					$(this).removeClass('focus');
+					$(this).parent().removeClass('focus');
+				if($(this).attr('data-type')=='pass'){
+					$(this).attr('type','text');
+				};
+			};
+		});
+	});
+	$('.form-input__viewpass').click(function(event) {
+		if($(this).hasClass('active')){
+			$(this).parent().find('input').attr('type','password');
+		}else{
+			$(this).parent().find('input').attr('type','text');
+		}
+		$(this).toggleClass('active');
+	});
+
+	// $('textarea').autogrow({vertical: true, horizontal: false});
---
+++ js/main.js
@@ -0,0 +1,4 @@
+//@prepros-append responsive.js
+//@prepros-append header-menu.js
+//@prepros-append hacks.js
+//@prepros-append datavalue.js
